d68a38bcc00eea2f1c0b13a00d361d968e72450a
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-20 01:05:07 +0700

    String Joiner Class

---
+++ src/rizaton/classes/StringJoinerApp.java
@@ -0,0 +1,17 @@
+package rizaton.classes;
+
+import java.util.StringJoiner;
+
+public class StringJoinerApp {
+    public static void main(String[] args) {
+
+        StringJoiner joiner = new StringJoiner(", ", "{", "}");
+
+        joiner.add("Iza");
+        joiner.add("Riza");
+        joiner.add("Rizaton");
+
+        String value = joiner.toString();
+        System.out.println(value);
+    }
+}
